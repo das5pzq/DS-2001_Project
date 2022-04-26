ce5005d2a48e21ae5589649b9ca38e5632e44ab5
das5pzq <81428951+das5pzq@users.noreply.github.com>
2022-04-25 20:40:31 -0400

    Add files via upload

--- Group_Project.py
+++ Group_Project.py
@@ -9,7 +9,7 @@ import pandas as pd
 import numpy as np
 import plotly
 import plotly.graph_objects as go
-data = pd.read_csv('Dataset.csv')
+data = pd.read_csv(r'C:\Users\xXDRP\DS_2001\DS_2001_Project\DS-2001_Project\Data.csv')
 
 data_broadband = data.dropna(subset = ['Country','Broadband Speed Rank','Broadband Mbps'])
 data_mobile = data.dropna(subset = ['Country','Mobile Speed Rank','Mobile Mbps'])
@@ -24,6 +24,18 @@ with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-c
 
 import plotly.express as px
 
-fig = px.choropleth(data_broadband, geojson=countries)
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+#fig = px.choropleth(data_broadband, geojson=countries)
+#fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+#fig.show()
+
+fig  = px.choropleth(data_broadband, 
+            locations='Country',
+             color="Broadband Mbps",
+            color_continuous_scale="Inferno",
+              locationmode="country names",
+               #scope="usa",
+             range_color=(0,100),
+             title='Global Broadband Speed Rank',
+             height=750)
+
 fig.show()
